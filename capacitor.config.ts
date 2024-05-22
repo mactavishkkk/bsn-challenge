091568c34ae5e25609e6f6277ef5f417e056236d
isaiasleite <isaiasleite230@gmail.com>
2024-05-22 15:33:03 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bns-challenge',
+  webDir: 'www'
+};
+
+export default config;
