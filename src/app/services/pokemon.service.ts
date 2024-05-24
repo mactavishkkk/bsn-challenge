d2606620391b2edb70ec559a90eed4bb0c571461
isaiasleite <isaiasleite230@gmail.com>
2024-05-24 00:12:47 -0400

    fix: refatoração do service - exibição dos detalhes

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,20 @@ import { Observable } from 'rxjs';
 export class PokemonService {
   private apiUrl = 'https://pokeapi.co/api/v2';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getAll(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/pokemon?limit=20`);
+  getAll(page: number = 1): Observable<any> {
+    const limit = 20;
+    const offset = (page - 1) * limit;
+    return this.http.get(`${this.apiUrl}/pokemon/?offset=${offset}&limit=${limit}`);
   }
-  
+
+  getDetails(id: number): Observable<any> {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon-form/${id}/`);
+  }
+
+  getAllWithDetails(ids: number[]): Observable<any[]> {
+    const requests = ids.map(id => this.getDetails(id));
+    return forkJoin(requests);
+  }
 }
