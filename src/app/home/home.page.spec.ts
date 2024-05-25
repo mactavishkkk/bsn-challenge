d510ef0c2d45582b1c1dccd017122b272dbf13ab
isaiasleite <isaiasleite230@gmail.com>
2024-05-25 01:04:46 -0400

    feat: testes de unidade

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,21 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
 import { HomePage } from './home.page';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PokemonService } from '../services/pokemon.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let mockPokeService: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    mockPokeService = jasmine.createSpyObj(['getAllWithDetails']);
+
+    TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: PokemonService, useValue: mockPokeService },
+        { provide: ActivatedRoute, useValue: { snapshot: { params: { id: '1' } } } }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
